aca142b77f1cc7fcc3641f6c93074e5832ecfbdb
UMAR SALMAN <32339184+umarsalman@users.noreply.github.com>
2018-10-01 17:47:13 +0530

    Update bomber-man.cpp

--- bomber-man.cpp
+++ bomber-man.cpp
@@ -1,20 +1,21 @@
+/*First four cases are different and they are repeated .*/
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 int main()
 {
-    int r,c;char *p1,*p2,*p3;
+    int r,c;
     ll n,i,j,l,m,k;
     cin>>r>>c>>n;
-    char arr[r+2][c+2]={'.'},cpy[r+2][c+2]={'.'};
-    
+    char arr[r+2][c+2]={'.'},cpy[r+2][c+2]={'.'};       /*cpy[][] is the copy of the array arr[][]*/
+                                                        /* One entire layer is to be left around the array so there is no problem when dealing at boundaries*/
     for(i=1;i<r+1;i++)
         for(j=1;j<c+1;j++)
         {
            cin>>arr[i][j];  
            cpy[i][j]=arr[i][j];
         }
-        if(n==1)
+        if(n==1)                                       /* If n==1 then array is to be printed as it is*/
     {
        for(i=1;i<r+1;i++)
        {
@@ -28,7 +29,7 @@ int main()
     }
     
     
-    if(n==2 || n==4)
+    if(n==2 || n==4)                            /* Only O's are supposed to be printed in this case*/
     {
        for(i=0;i<r;i++)
        {
@@ -43,7 +44,7 @@ int main()
 
     
     
-   if(n==3)
+   if(n==3)                                 /*Bomb bombartment takes places in this case*/
     {
             memset(arr,'O', sizeof(arr)); 
               for(k=1;k<r+1;k++)
@@ -69,8 +70,8 @@ int main()
    }
     
     
-    n=n%4;
-    if(n==2 || n==0)
+    n=n%4;                                            /*Now we just need to distinguish between four repeated cases*/
+    if(n==2 || n==0)                                  /* array with all O's needs to be printed in this case*/
     {
          for(i=0;i<r;i++)
        {
@@ -83,8 +84,8 @@ int main()
         return 0;
     }
    
-    if(n==3 || n==1)
-    {
+    if(n==3 || n==1)                              /*n==3 is similar to n==3 before the operation n=n%4*/
+    {                                             /*However for n==1 here we need to perform the same steps twice as did for n==3*/
            memset(arr,'O', sizeof(arr)); 
               for(k=1;k<r+1;k++)
               {
@@ -97,7 +98,7 @@ int main()
                 }
         }
     
-          for(k=1;k<r+1;k++)
+          for(k=1;k<r+1;k++)             /*Copying the array arr[][] into cpy[][]*/
               {
                   for(j=1;j<c+1;j++)
                 {
@@ -118,7 +119,7 @@ int main()
         
     
         
-          memset(arr,'O', sizeof(arr)); 
+          memset(arr,'O', sizeof(arr));                /*Repeating the same process once more*/
               for(k=1;k<r+1;k++)
               {
                   for(j=1;j<c+1;j++)
@@ -129,7 +130,7 @@ int main()
                  }
                 }
         }
-    copy(p1,p2,p3);
+   
     for(i=1;i<r+1;i++)
               {
                   for(j=1;j<c+1;j++)
